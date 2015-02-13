5822560f3032a3dcaa591f220cb259ac7f3de925
Carlos Nascimento <cnbatalha@gmail.com>
2015-02-13 12:56:01 -0400

    - Adicionando Item Ao grid

--- cadastro-angular-js/WebContent/exemplo/inicio.js
+++ cadastro-angular-js/WebContent/exemplo/inicio.js
@@ -12,4 +12,16 @@ function ListaComprasController($scope) {
 		quantidade : 1,
 		comprado : false
 	} ];
+
+	$scope.adicionarItem = function() {
+		$scope.itens.push({
+			produto : $scope.item.produto,
+			quantidade : $scope.item.quantidade,
+			comprado : false
+		});
+		
+		
+		$scope.item.produto = $scope.item.quantidade = '';
+
+	};
 }

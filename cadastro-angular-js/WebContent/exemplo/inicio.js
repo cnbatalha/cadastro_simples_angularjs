d7fa4250757ad7c32004819a6b36c382736d7a3d
Carlos Nascimento <cnbatalha@gmail.com>
2015-02-13 12:35:32 -0400

    - Adicionando table com lista de itens

---
+++ cadastro-angular-js/WebContent/exemplo/inicio.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+function ListaComprasController($scope) {
+	$scope.itens = [ {
+		produto : 'Leite',
+		quantidade : 2,
+		comprado : false
+	}, {
+		produto : 'Cerveja',
+		quantidade : 1,
+		comprado : false
+	} ];
+}

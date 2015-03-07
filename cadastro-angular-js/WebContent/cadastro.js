d900b6e885a2eaaabdeb701a822eabcdac52e49b
Carlos Nascimento <cnbatalha@gmail.com>
2015-03-07 10:37:13 -0400

    - Aplicando Roteamento AngularJS

--- cadastro-angular-js/WebContent/cadastro.js
+++ cadastro-angular-js/WebContent/cadastro.js
@@ -6,51 +6,33 @@ angular.module('cadastro', [ 'ngRoute' ]).config(function($routeProvider) {
 	}).when('/login', {
 		templateUrl : 'login.html',
 		controller : 'navigation'
+	}).when('/catequista', {
+		templateUrl : 'templates/catequista/catequista.html',
+		controller : 'catequistaController'
 	}).otherwise('/');
 
 }).controller('home', function($scope, $http) {
-	/*$http.get('/resource/').success(function(data) {
-		$scope.greeting = data;
-	});*/
-}).controller('navigation',function($rootScope, $scope, $http, $location) {
+	/*
+	 * $http.get('/resource/').success(function(data) { $scope.greeting = data;
+	 * });
+	 */
+}).controller('navigation', function($rootScope, $scope, $http, $location) {
 
-	/*var authenticate = function(callback) {
-
-		$http.get('user').success(function(data) {
-			if (data.name) {
-				$rootScope.authenticated = true;
-			} else {
-				$rootScope.authenticated = false;
-			}
-			callback && callback();
-		}).error(function() {
-			$rootScope.authenticated = false;
-			callback && callback();
-		});
-
-	}
-
-	authenticate();
-	$scope.credentials = {};
-	$scope.login = function() {
-		$http.post('login', $.param($scope.credentials), {
-			headers : {
-				"content-type" : "application/x-www-form-urlencoded"
-			}
-		}).success(function(data) {
-			authenticate(function() {
-				if ($rootScope.authenticated) {
-					$location.path("/");
-					$scope.error = false;
-				} else {
-					$location.path("/login");
-					$scope.error = true;
-				}
-			});
-		}).error(function(data) {
-			$location.path("/login");
-			$scope.error = true;
-			$rootScope.authenticated = false;
-		})
-	};*/
+	/*
+	 * var authenticate = function(callback) {
+	 * 
+	 * $http.get('user').success(function(data) { if (data.name) {
+	 * $rootScope.authenticated = true; } else { $rootScope.authenticated =
+	 * false; } callback && callback(); }).error(function() {
+	 * $rootScope.authenticated = false; callback && callback(); }); }
+	 * 
+	 * authenticate(); $scope.credentials = {}; $scope.login = function() {
+	 * $http.post('login', $.param($scope.credentials), { headers : {
+	 * "content-type" : "application/x-www-form-urlencoded" }
+	 * }).success(function(data) { authenticate(function() { if
+	 * ($rootScope.authenticated) { $location.path("/"); $scope.error = false; }
+	 * else { $location.path("/login"); $scope.error = true; } });
+	 * }).error(function(data) { $location.path("/login"); $scope.error = true;
+	 * $rootScope.authenticated = false; }) };
+	 */
 });

6fce395545f15cdb53f4351d5bacfbec4312b59c
Carlos Nascimento <cnbatalha@gmail.com>
2015-07-17 11:38:19 -0400

    - configurando archetype:create-from-project

---
+++ cadastro-ajs-server/archetype-create-from-project.bat
@@ -0,0 +1,4 @@
+mvn archetype:create-from-project
+
+// executar na pasta archetype em target
+mvn install
--- cadastro-ajs-server/src/main/java/com/cadastro/security/AuthenticationSucessHandler.java
+++
@@ -1,5 +0,0 @@
-package com.cadastro.security;
-
-public class AuthenticationSucessHandler {
-
-}

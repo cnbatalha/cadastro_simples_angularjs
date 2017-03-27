f6e0bcc5509e10751701108d8e404c48a46afecf
Carlos Nascimento <cnbatalha@gmail.com>
2017-03-27 17:39:42 -0400

    add projeto app notas

---
+++ app-notas/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appNotasApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appNotasApp
+ */
+angular.module('appNotasApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
